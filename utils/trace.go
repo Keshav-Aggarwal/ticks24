3f1c85b57b51b9a9651ba15ad8c9b7cbd3d5239f
vivekyadav.jit@gmail.com <vivekyadav.jit@gmail.com>
2016-09-26 10:33:11 +0530

    Trace and Un Added to utils

---
+++ utils/trace.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"log"
+	"time"
+)
+
+func Trace(s string) (string, time.Time) {
+	log.Println("START:", s)
+	return s, time.Now()
+}
+
+func Un(s string, startTime time.Time) {
+	endTime := time.Now()
+	log.Println("  END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime))
+}
